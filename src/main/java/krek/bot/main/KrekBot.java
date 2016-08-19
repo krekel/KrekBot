a12b2e9d607298420d47e734f7afbd6293fbff5e
krekel <breakingloco15@gmail.com>
2016-08-19 19:48:39 -0400

    Re-writing the project. Many fails, but whatever.

--- src/main/java/krek/bot/main/KrekBot.java
+++ src/main/java/krek/bot/main/KrekBot.java
@@ -1,8 +1,6 @@
 package krek.bot.main;
 
 import krek.bot.command.CommandHandler;
-import krek.bot.giveaway.GiveawayEvent;
-import krek.bot.giveaway.GiveawayHandler;
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.util.DiscordException;
@@ -15,11 +13,16 @@ public class KrekBot {
 		client = getClient("Token");
 		client.getDispatcher().registerListener(new ReadyListener());
 		client.getDispatcher().registerListener(new CommandHandler());
-        client.getDispatcher().registerListener(new GiveawayHandler());
 	}
 
-	private static IDiscordClient getClient(String token) throws DiscordException {
-		return new ClientBuilder().withToken(token).login();
+	private static IDiscordClient getClient(String token) {
+		try {
+			client = new ClientBuilder().withToken(token).login();
+		} catch (DiscordException e){
+			System.out.println("Error while logging in.");
+			e.printStackTrace();
+		}
+		return client;
 	}
 	
 	public static IDiscordClient getClient(){
