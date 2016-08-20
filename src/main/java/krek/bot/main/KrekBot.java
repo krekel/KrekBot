5b2981b7096becba49bf40507c753c4f5e1c08e3
Luis Diaz <breakingloco15@gmail.com>
2016-08-20 12:38:29 -0400

    Re-writing the project. Many fails, but whatever.

--- src/main/java/krek/bot/main/KrekBot.java
+++ src/main/java/krek/bot/main/KrekBot.java
@@ -1,31 +1,51 @@
 package krek.bot.main;
 
 import krek.bot.command.CommandHandler;
+import org.ini4j.InvalidFileFormatException;
+import org.ini4j.Wini;
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.util.DiscordException;
 
+import java.io.File;
+import java.io.IOException;
+
 public class KrekBot {
 
-	public static IDiscordClient client;
+	static IDiscordClient client;
+    static String TOKEN;
 
-	public static void main(String[] args) throws DiscordException  {
-		client = getClient("Token");
+
+	public static void main(String[] args) {
+		client = getClient();
 		client.getDispatcher().registerListener(new ReadyListener());
 		client.getDispatcher().registerListener(new CommandHandler());
 	}
 
-	private static IDiscordClient getClient(String token) {
+	private static void login(String token) {
 		try {
 			client = new ClientBuilder().withToken(token).login();
 		} catch (DiscordException e){
 			System.out.println("Error while logging in.");
 			e.printStackTrace();
 		}
-		return client;
 	}
 	
 	public static IDiscordClient getClient(){
-		return client;
+
+        return client;
 	}
+
+    private static void getToken(){
+        try {
+            Wini ini = new Wini(new File("C:\\KrekBot\\BotSettings.ini"));
+            TOKEN = ini.get("Credentials", "Token");
+        } catch (InvalidFileFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
